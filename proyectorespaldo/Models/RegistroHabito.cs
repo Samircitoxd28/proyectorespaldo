1d641e26437e671b55aac22b6b8ef66f7c5702a0
Samircitoxd28 <kleber.mosquera@outlook.com>
2025-02-23 23:36:17 -0500

    Agregar archivos de proyecto.

---
+++ proyectorespaldo/Models/RegistroHabito.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace proyectorespaldo.Models;
+
+public partial class RegistroHabito
+{
+    public int IdRegistro { get; set; }
+
+    public int IdHabito { get; set; }
+
+    public DateOnly? Fecha { get; set; }
+
+    public string? Estado { get; set; }
+
+    public virtual Habito IdHabitoNavigation { get; set; } = null!;
+}
---
+++ proyectorespaldo/Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace proyectorespaldo.Models;
+
+public partial class Usuario
+{
+    public int IdUsuario { get; set; }
+
+    public string NombreCompleto { get; set; } = null!;
+
+    public string Correo { get; set; } = null!;
+
+    public string Contraseña { get; set; } = null!;
+
+    public string? Genero { get; set; }
+
+    public int? Edad { get; set; }
+
+    public DateTime? FechaRegistro { get; set; }
+
+    public bool? DarkMode { get; set; }
+
+    public virtual ICollection<Habito> Habitos { get; set; } = new List<Habito>();
+}
